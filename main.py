c0e3391ff40995618a9d0455d739c675a52384f7
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-06 03:37:24 +0200

    the main file

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
+# import libraries and Classes
 from selenium import webdriver
-from time import process_time
 import os
 import urllib.request
 
-driver = webdriver.Chrome("C:\Program Files\chromedriver.exe")
+driver = webdriver.Chrome("C:\Program Files\chromedriver.exe") #Put the path of Your webdriver
 
-
-# getting the data (src and name of the logo and his link)
-cars_list = []
-for i in range(1, 2):
-   page = "https://www.carlogos.org/car-brands/list_1_{}.html".format(i)
-   driver.get(page)
-   cars = driver.find_element_by_class_name('logo-list')
-   car_links = cars.find_elements_by_tag_name('a')
-   cars_images = driver.find_elements_by_tag_name('img')
-   j = 0
-   for image in cars_images:
-       if j < len(car_links):
-           cars_data = []
-           cars_data.append(image.get_attribute('src'))
-           cars_data.append(image.get_attribute('alt'))
-           cars_data.append(car_links[j].get_attribute('href'))
-           if cars_data not in cars_list:
-               cars_list.append(cars_data)
-           j += 1
-
-# Making Directories of such brand and Download all the logos
-# creating file contain list of all brands
-car_file = open("list of cars.txt", "w")
-for car in cars_list:
-    car_brand = car[1][:-5]
-    car_file.write(car_brand+'\n')
-    os.mkdir('{}'.format(car_brand))
-    urllib.request.urlretrieve(url = car[0], filename = '{}\{}.png'.format(car_brand, car[1]))
-
+def get_source():
+    cars_list = []
+    for i in range(1, 9):
+        page = "https://www.carlogos.org/car-brands/list_1_{}.html".format(i)
+        driver.get(page)
+        cars = driver.find_element_by_class_name('logo-list')
+        cars_links = cars.find_elements_by_tag_name('a')
+        cars_logos = cars.find_elements_by_tag_name('img')
+        for j in range(len(cars_links)):
+            car_data = []
+            car_data.append(cars_links[j].get_attribute('href'))
+            car_data.append(cars_logos[j].get_attribute('src'))
+            cars_list.append(car_data)
+    return (cars_list)
 
 def clean_car_data(data):
     data_lines = []
@@ -58,24 +43,34 @@ def clean_car_data(data):
             clean_data.append(y)
     return (clean_data)
 
-# Downloading All the Brands Review
-for car_link in cars_list:
-    driver.get(car_link[2])
-    car_title = driver.find_element_by_class_name('title').find_element_by_tag_name('h1').get_attribute('innerHTML')
-    car_title = car_title[:-5]
-    try:
-        car_information = driver.find_element_by_class_name('brand-overview').find_element_by_class_name(
-            'fold-text').get_attribute('innerHTML')
-    except:
-        car_information = driver.find_element_by_tag_name('tbody').get_attribute('innerHTML')
-
-    car_data = clean_car_data(car_information)
-    car_data_file = open('{}\{}.txt'.format(car_title, car_title + ' data'), 'w')
-    for x in car_data:
+def get_data(cars_list):
+    car_file = open("list of cars.txt", "w")
+    car_file.write('The list of cars: \n')
+    for car in cars_list:
+        driver.get(car[0])
+        car_brand = driver.find_element_by_class_name('title').find_element_by_tag_name('h1').get_attribute('innerHTML')
+        car_brand = car_brand[:-5]
+        if car_brand[-1] == ' ':
+            car_brand = car_brand[:car_brand.rindex(' ')]
         try:
-            car_data_file.write(x + '\n')
+            car_information = driver.find_element_by_class_name('brand-overview').find_element_by_class_name(
+                'fold-text').get_attribute('innerHTML')
         except:
-            car_data_file.write('invalid data')
+            car_information = driver.find_element_by_tag_name('tbody').get_attribute('innerHTML')
+        car_information = clean_car_data(car_information)
+        os.mkdir('{}'.format(car_brand))
+        urllib.request.urlretrieve(url=car[1], filename='{}\{}.png'.format(car_brand, car_brand + ' Logo'))
+        car_data_file = open('{}\{}.txt'.format(car_brand, car_brand + ' Review'), 'w')
+        test_2 = True
+        for line in car_information:
+            if test_2:
+                try:
+                    car_data_file.write(line + '\n')
+                except:
+                    car_data_file.write('invalid data')
+                    test_2 = False
+        car_data_file.write('{}\n'.format(car_brand))
 
-print("scrapping the data takes: ".format(process_time()))
 
+cars_list = get_source()
+get_data(cars_list)
